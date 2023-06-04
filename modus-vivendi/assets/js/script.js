b9fdd28a01939c573cb142989ffd2a99bbca904e
Panthor <7573793+Panthor@users.noreply.github.com>
2023-06-04 14:44:39 +0200

    Delete modus-vivendi directory

--- modus-vivendi/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function () {
-  var sukniheader = document.getElementById("l-header");
-  var mejajslika = document.getElementById("slika");
-  sukniheader.classList.toggle("sticky", window.scrollY > 0);
-  mejajslika.classList.toggle("manja", window.scrollY > 0);
-});
